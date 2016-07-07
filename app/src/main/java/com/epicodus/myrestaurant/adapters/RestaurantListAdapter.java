8cfc9f9b85f592ce7a6070efe359ac42de01012f
Andrew Russo and Kyle Wolfson <andrew@>
2016-07-07 10:40:20 -0700

    Creade functioning fragment pager adapter, and will work to have it displaying restaurant info next.

--- app/src/main/java/com/epicodus/myrestaurant/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurant/adapters/RestaurantListAdapter.java
@@ -1,6 +1,7 @@
 package com.epicodus.myrestaurant.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,10 @@ import android.widget.TextView;
 
 import com.epicodus.myrestaurant.R;
 import com.epicodus.myrestaurant.models.Restaurant;
+import com.epicodus.myrestaurant.ui.RestaurantDetailActivity;
 import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -48,7 +52,7 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
     }
 
 
-    public class RestaurantViewHolder extends RecyclerView.ViewHolder {
+    public class RestaurantViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         @Bind(R.id.restaurantImageView) ImageView mRestaurantImageView;
         @Bind(R.id.restaurantNameTextView) TextView mNameTextView;
         @Bind(R.id.categoryTextView) TextView mCategoryTextView;
@@ -59,6 +63,16 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
             super(itemView);
             ButterKnife.bind(this, itemView);
             mContext = itemView.getContext();
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int itemPosition = getLayoutPosition();
+            Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
+            intent.putExtra("position", itemPosition + "");
+            intent.putExtra("restaurants", Parcels.wrap(mRestaurants));
+            mContext.startActivity(intent);
         }
 
         public void bindRestaurant(Restaurant restaurant) {
--- app/src/main/java/com/epicodus/myrestaurant/models/Restaurant.java
+++ app/src/main/java/com/epicodus/myrestaurant/models/Restaurant.java
@@ -2,19 +2,24 @@ package com.epicodus.myrestaurant.models;
 
 import java.util.ArrayList;
 
+import org.parceler.Parcel;
+
 /**
  * Created by Guest on 7/5/16.
  */
+@Parcel
 public class Restaurant {
-    private String mName;
-    private String mPhone;
-    private String mWebsite;
-    private double mRating;
-    private String mImageUrl;
-    private ArrayList<String> mAddress = new ArrayList<>();
-    private double mLatitude;
-    private double mLongitude;
-    private ArrayList<String> mCategories = new ArrayList<>();
+     String mName;
+     String mPhone;
+     String mWebsite;
+     double mRating;
+     String mImageUrl;
+     ArrayList<String> mAddress = new ArrayList<>();
+     double mLatitude;
+     double mLongitude;
+     ArrayList<String> mCategories = new ArrayList<>();
+
+    public Restaurant() {}
 
     public Restaurant(String name, String phone, String website,
                       double rating, String imageUrl, ArrayList<String> address,
