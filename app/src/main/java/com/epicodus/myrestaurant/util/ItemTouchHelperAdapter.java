ba676769e338821bcb98390812cad1943c4da4b4
Andrew Russo and Jeffrey Hackford <jeffrey@>
2016-07-18 09:20:46 -0700

    Set up app to allow gestures and added icons to indicate that saved items can be dragged and rearranged.

---
+++ app/src/main/java/com/epicodus/myrestaurant/util/ItemTouchHelperAdapter.java
@@ -0,0 +1,9 @@
+package com.epicodus.myrestaurant.util;
+
+/**
+ * Created by Guest on 7/18/16.
+ */
+public interface ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPostition);
+    void onItemDismess(int position);
+}
